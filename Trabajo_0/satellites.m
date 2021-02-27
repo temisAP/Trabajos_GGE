f4e49f5f12457bf091c58cd7154ee9cda9c68b3e
temisAP <74715759+temisAP@users.noreply.github.com>
2021-02-27 17:56:43 +0100

    updated table

--- Trabajo_0/satellites.m
+++ Trabajo_0/satellites.m
@@ -2,24 +2,31 @@ close all
 format short
 filename='satellites.xls';
 
+%% Read data
+
 data = xlsread(filename);
 mass = data(2:end,1);
+power = data(2:end,2);
+capacity = data(2:end,3);
+
+%% Interpolation
+
 for i=2:length(mass)
     if mass(i) <= mass(i-1)
-        mass(i) = mass(i-1)+1e-6;
+        mass(i) = mass(i-1)+1e-6; %To avoid non-unique values
     end
 end
 massq = linspace(0,max(mass)+10,100);
 
-power = data(2:end,2);
-powerfit = fit(mass,power,'poly1');
+[powerfit, powergof] = fit(mass,power,'poly1');
 power_coeff = coeffvalues(powerfit);
 powerq = power_coeff(1)*massq + power_coeff(2);
 
-capacity = data(2:end,3);
-capacityfit = fit(mass,capacity,'poly1');
+[capacityfit, capacitygof] = fit(mass,capacity,'poly1');
 capacity_coeff = coeffvalues(capacityfit);
 capacityq = capacity_coeff(1)*massq + capacity_coeff(2);
+
+%% Graphical representation
 
 h = figure();
     hold on
@@ -38,7 +45,7 @@ h = figure();
     hold on
     scatter(mass, capacity, 'k','DisplayName', 'Datos')
     plot(massq, capacityq, '-', 'LineWidth', 2, 'Color', 'k','DisplayName', ...
-        ['P = ',num2str(capacity_coeff(1)),' m + ', num2str(capacity_coeff(2))])
+        ['C = ',num2str(capacity_coeff(1)),' m + ', num2str(capacity_coeff(2))])
     box on; grid on
     legend('Interpreter', 'Latex', 'location', 'best')
     title("\textit{\textbf{Capacidad vs masa}}",'Interpreter','latex')
