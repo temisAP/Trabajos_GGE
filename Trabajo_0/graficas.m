b23296af484b86e71fe7d8f85bb7d38458cbfeeb
Danieldelriovelilla <daniel_drv@hotmail.es>
2021-02-28 10:52:52 +0100

    cambio figuras

--- Trabajo_0/graficas.m
+++ Trabajo_0/graficas.m
@@ -1,3 +1,7 @@
+clc;
+clear all;
+close all;
+
 x = linspace(0,100,101);
 y = x;
 
@@ -9,7 +13,10 @@ h = figure();
     plot(x, -y, ':', 'LineWidth', 2, 'Color', 'k', 'DisplayName', 'Curva 4')
     box on; grid on
     legend('Interpreter', 'Latex', 'location', 'best')    % location: NorthEast
-    title("\textit{\textbf{Prueba im\'agen}}",'Interpreter','latex')
-    xlabel('$x$ [m]','Interpreter','latex')
-    ylabel({'$y$';'[m]'},'Interpreter','latex')
+    % title("\textit{\textbf{Prueba im\'agen}}",'Interpreter','latex')
+    xlh = xlabel('$x$ [m]','Interpreter','latex')
+    xlh.Position(1) = xlh.Position(1) + 42.5;
+    ylh = ylabel({'$y$';'[m]'},'Interpreter','latex');
+    ylh.Position(1) = ylh.Position(1) - 3;   % abs(ylh.Position(1) * 0.3)
+    ylh.Position(2) = ylh.Position(2) + 75;
     Save_as_PDF(h, 'test',0);    % Save_as_PDF(h, 'Figuras/test',0)
