939ab27f7703c406bb61cc37537fbc672522c8d4
Davoestebanez <64979617+Davoestebanez@users.noreply.github.com>
2021-03-15 23:19:47 +0100

    Cosas nuevas
    Numeric actualizado
    Creación de archivo data.mat
    Nuevas figuras

--- Trabajo_2/numeric.m
+++ Trabajo_2/numeric.m
@@ -2,47 +2,64 @@ clear all;
 close all;
 clc;
 
-for s = 1:8
-    % Nombre de las hojas del archivo excel
-    sheet = {'RTC France', 'TNJ', 'ZTJ', '3G30C','PWP201', 'KC200GT2', 'SPVSX5', 'PSC'};
+% Nombre de las hojas del archivo excel
+sheet = {'RTC France', 'TNJ', 'ZTJ', '3G30C','PWP201', 'KC200GT2', 'SPVSX5', 'PSC'};
 
-    % Selección de hoja (s)
-    % 1 ---> RTC France
-    % 2 ---> TNJ
-    % 3 ---> ZTJ
-    % 4 ---> 3G30C
-    % 5 ---> PWP201
-    % 6 ---> KC200GT2
-    % 7 ---> SPVSX5
-    % 8 ---> PSC
+% Selección de hoja (s)
+% 1 ---> RTC France
+% 2 ---> TNJ
+% 3 ---> ZTJ
+% 4 ---> 3G30C
+% 5 ---> PWP201
+% 6 ---> KC200GT2
+% 7 ---> SPVSX5
+% 8 ---> PSC
+
+% % Carga de valores experimentales y datos del fabricante
+% for s = 1:8
+%     data{s,1} = xlsread('IV_curves.xlsx', sheet{s}, 'A21:A1202');
+%     data{s,2} = xlsread('IV_curves.xlsx', sheet{s}, 'B21:B1202');
+%     data{s,3} = xlsread('IV_curves.xlsx', sheet{s}, 'B1');
+%     data{s,4} = xlsread('IV_curves.xlsx', sheet{s}, 'B2');
+%     data{s,5} = xlsread('IV_curves.xlsx', sheet{s}, 'B3');
+%     data{s,6} = xlsread('IV_curves.xlsx', sheet{s}, 'B4');
+%     data{s,7} = xlsread('IV_curves.xlsx', sheet{s}, 'B5');
+%     data{s,8} = xlsread('IV_curves.xlsx', sheet{s}, 'B6');
+% end
+
+% Cargar datos de archivo .mat
+load('data.mat');
+
+
+for s = 4 
     
     % Carga de valores experimentales
-    V_mess = xlsread('IV_curves.xlsx', sheet{s}, 'A21:A1202');
-    I_mess = xlsread('IV_curves.xlsx', sheet{s}, 'B21:B1202');
+    V_mess = data{s,1};
+    I_mess = data{s,2};
 
     % Carga de datos del fabricante
-    Isc = xlsread('IV_curves.xlsx', sheet{s}, 'B1');
-    Imp = xlsread('IV_curves.xlsx', sheet{s}, 'B2');
-    Vmp = xlsread('IV_curves.xlsx', sheet{s}, 'B3');
-    Voc = xlsread('IV_curves.xlsx', sheet{s}, 'B4');
-    betha = xlsread('IV_curves.xlsx', sheet{s}, 'B5');
-    alpha = xlsread('IV_curves.xlsx', sheet{s}, 'B6');
+    Isc = data{s,3};
+    Imp = data{s,4};
+    Vmp = data{s,5};
+    Voc = data{s,6};
+    betha = data{s,7};
+    alpha = data{s,8};
 
 
     % Plot de valores experimentales
-    h_ = figure(1);
-        hold on
-        plot(V_mess, I_mess, '-.', 'LineWidth', 2, 'Color', 'k','DisplayName', ...
-            ["Valores experimentales"])
-        plot([0 Vmp Voc], [Isc Imp 0], 'o', 'MarkerSize', 10, 'Color', 'k',...
-            'DisplayName', ["Datos fabricante"])
-        axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
-        box on; grid on
-        legend('Interpreter', 'Latex', 'location', 'Best')
-        xlabel('$V$ [V]','Interpreter','latex');
-        ylabel({'$I$';'[A]'},'Interpreter','latex');
-        Save_as_PDF(h_, ['Figuras/valores_exp_', sheet{s}],'horizontal');
-        hold off
+%     h_ = figure(1);
+%         hold on
+%         plot(V_mess, I_mess, '-.', 'LineWidth', 2, 'Color', 'k','DisplayName', ...
+%             ["Valores experimentales"])
+%         plot([0 Vmp Voc], [Isc Imp 0], 'o', 'MarkerSize', 10, 'Color', 'k',...
+%             'DisplayName', ["Datos fabricante"])
+%         axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
+%         box on; grid on
+%         legend('Interpreter', 'Latex', 'location', 'Best')
+%         xlabel('$V$ [V]','Interpreter','latex');
+%         ylabel({'$I$';'[A]'},'Interpreter','latex');
+%         Save_as_PDF(h_, ['Figuras/valores_exp_', sheet{s}],'horizontal');
+%         hold off
 
     %% Karmalkar & Haneefa numérico
 
@@ -66,39 +83,39 @@ for s = 1:8
 
     I_Ksol = (1-(1-gamma(end))*v_mess - gamma(end)*v_mess.^m(end))*Isc;
 
-    h_ = figure(2);
-        hold on
-        plot(V_mess, I_mess, '-.', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
-            ["Valores experimentales"])
-        plot(V_mess, I_Ksol, '-', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
-            ["Karmalkar \& Haneefa num\'erico"])
-        axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
-        box on; grid on
-        legend('Interpreter', 'Latex', 'location', 'Best')
-        xlabel('$V$ [V]','Interpreter','latex');
-        ylabel({'$I$';'[A]'},'Interpreter','latex');
-        Save_as_PDF(h_, ['Figuras/1_Nu_KyH_', sheet{s}],'horizontal');
-        hold off
-    
-    save_filename = 'numeric.xlsx';
-    save_sheet = 'KyH';
-    
-    % Name
-    pos = strjoin({'A',num2str(s+1)},'');
-    A = cellstr(sheet{s});
-    xlswrite(save_filename,A,save_sheet,pos);
-    % m
-    pos = strjoin({'B',num2str(s+1)},'');
-    A = round(m(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
-    % gamma
-    pos = strjoin({'C',num2str(s+1)},'');
-    A = round(gamma(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
-    % Error_K
-    pos = strjoin({'D',num2str(s+1)},'');
-    A = round(Error_K(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
+%     h_ = figure(2);
+%         hold on
+%         plot(V_mess, I_mess, '-.', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
+%             ["Valores experimentales"])
+%         plot(V_mess, I_Ksol, '-', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
+%             ["Karmalkar \& Haneefa num\'erico"])
+%         axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
+%         box on; grid on
+%         legend('Interpreter', 'Latex', 'location', 'Best')
+%         xlabel('$V$ [V]','Interpreter','latex');
+%         ylabel({'$I$';'[A]'},'Interpreter','latex');
+%         Save_as_PDF(h_, ['Figuras/1_Nu_KyH_', sheet{s}],'horizontal');
+%         hold off
+%     
+%     save_filename = 'numeric.xlsx';
+%     save_sheet = 'KyH';
+%     
+%     % Name
+%     pos = strjoin({'A',num2str(s+1)},'');
+%     A = cellstr(sheet{s});
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % m
+%     pos = strjoin({'B',num2str(s+1)},'');
+%     A = round(m(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % gamma
+%     pos = strjoin({'C',num2str(s+1)},'');
+%     A = round(gamma(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % Error_K
+%     pos = strjoin({'D',num2str(s+1)},'');
+%     A = round(Error_K(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
 
 
     %% Das numérico
@@ -120,39 +137,39 @@ for s = 1:8
 
     I_Dsol = (1-v_mess.^k(end))./(1+h(end)*v_mess)*Isc;
 
-    h_ = figure(3);
-        hold on
-        plot(V_mess, I_mess, '-.', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
-            ["Valores experimentales"])
-        plot(V_mess, I_Dsol, '-', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
-            ["Das num\'erico"])
-        axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
-        box on; grid on
-        legend('Interpreter', 'Latex', 'location', 'Best')
-        xlabel('$V$ [V]','Interpreter','latex');
-        ylabel({'$I$';'[A]'},'Interpreter','latex');
-        Save_as_PDF(h_, ['Figuras/1_Nu_Das_', sheet{s}],'horizontal');
-        hold off
-        
-    save_filename = 'numeric.xlsx';
-    save_sheet = 'Das';
-    
-    % Name
-    pos = strjoin({'A',num2str(s+1)},'');
-    A = cellstr(sheet{s});
-    xlswrite(save_filename,A,save_sheet,pos);
-    % k_Das
-    pos = strjoin({'B',num2str(s+1)},'');
-    A = round(k(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
-    % h
-    pos = strjoin({'C',num2str(s+1)},'');
-    A = round(h(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
-    % Error_D
-    pos = strjoin({'D',num2str(s+1)},'');
-    A = round(Error_D(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
+%     h_ = figure(3);
+%         hold on
+%         plot(V_mess, I_mess, '-.', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
+%             ["Valores experimentales"])
+%         plot(V_mess, I_Dsol, '-', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
+%             ["Das num\'erico"])
+%         axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
+%         box on; grid on
+%         legend('Interpreter', 'Latex', 'location', 'Best')
+%         xlabel('$V$ [V]','Interpreter','latex');
+%         ylabel({'$I$';'[A]'},'Interpreter','latex');
+%         Save_as_PDF(h_, ['Figuras/1_Nu_Das_', sheet{s}],'horizontal');
+%         hold off
+%         
+%     save_filename = 'numeric.xlsx';
+%     save_sheet = 'Das';
+%     
+%     % Name
+%     pos = strjoin({'A',num2str(s+1)},'');
+%     A = cellstr(sheet{s});
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % k_Das
+%     pos = strjoin({'B',num2str(s+1)},'');
+%     A = round(k(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % h
+%     pos = strjoin({'C',num2str(s+1)},'');
+%     A = round(h(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % Error_D
+%     pos = strjoin({'D',num2str(s+1)},'');
+%     A = round(Error_D(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
 
     %% Pindado & Cubas numérico
 
@@ -182,40 +199,49 @@ for s = 1:8
 
     I_PCsol = [I_tramo1' I_tramo2'];
 
-    h_ = figure(4);
-        hold on
-        plot(V_mess, I_mess, '-.', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
-            ["Valores experimentales"])
-        plot(V_mess, I_PCsol, '-', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
-            ["Pindado \& Cubas num\'erico"])
-        axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
-        box on; grid on
-        legend('Interpreter', 'Latex', 'location', 'Best')
-        xlabel('$V$ [V]','Interpreter','latex');
-        ylabel({'$I$';'[A]'},'Interpreter','latex');
-        Save_as_PDF(h_, ['Figuras/1_Nu_PyC_', sheet{s}],'horizontal');
-        hold off
-        
-    save_filename = 'numeric.xlsx';
-    save_sheet = 'PyC';
     
-    % Name
-    pos = strjoin({'A',num2str(s+1)},'');
-    A = cellstr(sheet{s});
-    xlswrite(save_filename,A,save_sheet,pos);
-    % phi
-    pos = strjoin({'B',num2str(s+1)},'');
-    A = round(phi(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
-    % Error_PC
-    pos = strjoin({'C',num2str(s+1)},'');
-    A = round(Error_PC(i),3,'significant');
-    xlswrite(save_filename,A,save_sheet,pos);
+%     h_ = figure(4);
+%         hold on
+%         plot(V_mess, I_mess, '-.', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
+%             ["Valores experimentales"])
+%         plot(V_mess, I_PCsol, '-', 'LineWidth', 1.5, 'Color', 'k','DisplayName', ...
+%             ["Pindado \& Cubas num\'erico"])
+%         axis([0, V_mess(end)*1.1, 0, I_mess(1)*1.1])
+%         box on; grid on
+%         legend('Interpreter', 'Latex', 'location', 'Best')
+%         xlabel('$V$ [V]','Interpreter','latex');
+%         ylabel({'$I$';'[A]'},'Interpreter','latex');
+%         Save_as_PDF(h_, ['Figuras/1_Nu_PyC_', sheet{s}],'horizontal');
+%         hold off
+%         
+%     save_filename = 'numeric.xlsx';
+%     save_sheet = 'PyC';
+%     
+%     % Name
+%     pos = strjoin({'A',num2str(s+1)},'');
+%     A = cellstr(sheet{s});
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % phi
+%     pos = strjoin({'B',num2str(s+1)},'');
+%     A = round(phi(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
+%     % Error_PC
+%     pos = strjoin({'C',num2str(s+1)},'');
+%     A = round(Error_PC(i),3,'significant');
+%     xlswrite(save_filename,A,save_sheet,pos);
+
+    %% Plot final
 
     h_ = figure(5);
         hold on
-        plot(V_mess, I_mess, '-.', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
-            ["Valores experimentales"])
+        % Plotear solo ciertos puntos
+        if (s == 4 | s == 7)
+            plot(V_mess(1:10:end)', I_mess(1:10:end)', '-o', 'LineWidth', 1,...
+            'Color', 'k','DisplayName', ["Valores experimentales"])
+        else
+            plot(V_mess, I_mess, '-o', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
+                ["Valores experimentales"])
+        end
         plot(V_mess, I_Ksol, '-', 'LineWidth', 1, 'Color', 'r','DisplayName', ...
             ["Karmalkar \& Haneefa num\'erico"])
         plot(V_mess, I_Dsol, '-', 'LineWidth', 1, 'Color', 'b','DisplayName', ...
@@ -229,10 +255,11 @@ for s = 1:8
         ylabel({'$I$';'[A]'},'Interpreter','latex');
         Save_as_PDF(h_, ['Figuras/1_Nu_', sheet{s}],'horizontal');
         hold off
-    
-    if s ~= 8
-        clear all;
-        close all;
-    end
+   
+        
+%     if (s ~= 8)
+%         close all;
+%     end
 
 end
+
