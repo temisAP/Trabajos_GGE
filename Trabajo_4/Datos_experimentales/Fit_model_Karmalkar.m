4ab9738099bf10cbb9e5dae9c796a8c766c6824b
Davoestebanez <64979617+Davoestebanez@users.noreply.github.com>
2021-04-11 14:07:23 +0200

    Figuras apartado 1 cambiadas

--- Trabajo_4/Datos_experimentales/Fit_model_Karmalkar.m
+++ Trabajo_4/Datos_experimentales/Fit_model_Karmalkar.m
@@ -64,70 +64,62 @@ I_Karmalkar_analytic_datasheet = (1-(1-gamma)*(V_mess/Voc)-gamma*(V_mess/Voc).^m
 %% FIGURAS ERROR
 
 h_ = figure(1);
-hold on
-plot(V_mess, abs(I_Karmalkar_analytic_exp-I_mess), '-', 'LineWidth', 1, 'Color', 'k')
-box on; grid on
-xlabel('$V$ [V]','Interpreter','latex');
-ylabel({'$I-I_{exp}$';'[A]'},'Interpreter','latex');
-Save_as_PDF(h_, ['Figuras/Error_KyH_exp'],'horizontal', 7.5, 10);
+    hold on
+    plot(V_mess, abs(I_Karmalkar_analytic_exp-I_mess), '-', 'LineWidth', 1, 'Color', 'k')
+    box on; grid on
+    xlabel('$V$ [V]','Interpreter','latex');
+    ylabel({'$I-I_{exp}$';'[A]'},'Interpreter','latex');
+    %Save_as_PDF(h_, ['Figuras/Error_KyH_exp'],'horizontal', 7.5, 10);
 
 h_ = figure(2);
-hold on
-plot(V_mess, abs(I_Karmalkar_analytic_datasheet-I_mess), '-', 'LineWidth', 1, 'Color', 'k')
-box on; grid on
-xlabel('$V$ [V]','Interpreter','latex');
-ylabel({'$I-I_{exp}$';'[A]'},'Interpreter','latex');
-Save_as_PDF(h_, ['Figuras/Error_KyH_datasheet'],'horizontal', 7.5, 10);
-
-% h_ = figure(3);
-% hold on
-% plot(V_mess, abs(I_Karmalkar_analytic_datasheet-I_mess), '-', 'LineWidth', 1, 'Color', 'k')
-% box on; grid on
-% xlabel('$V$ [V]','Interpreter','latex');
-% ylabel({'$I-I_{exp}$';'[A]'},'Interpreter','latex');
-% Save_as_PDF(h_, ['Figuras/Error_KyH_datasheet'],'horizontal', 7.5, 10);
+    hold on
+    plot(V_mess, abs(I_Karmalkar_analytic_datasheet-I_mess), '-', 'LineWidth', 1, 'Color', 'k')
+    box on; grid on
+    xlabel('$V$ [V]','Interpreter','latex');
+    ylabel({'$I-I_{exp}$';'[A]'},'Interpreter','latex');
+    %Save_as_PDF(h_, ['Figuras/Error_KyH_datasheet'],'horizontal', 7.5, 10);
 
 
 %% FIGURAS CURVAS
-% h_ = figure(1);
-%     hold on 
-%     plot(V_mess(1:25:end), I_mess(1:25:end), 'o', 'LineWidth', 0.5, 'MarkerSize', 5, 'Color', 'k','DisplayName', ...
-%         ["Datos experimentales"])
-%     plot(puntos_x_datos, puntos_y_datos, '^', 'Linewidth', 1,'Color', 'k', 'DisplayName', ["Puntos caracter\'isticos: datos experimentales"])
-%     plot(V_mess, I_Karmalkar_analytic_exp, '-', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
-%         ["Karmalkar \& Haneefa: datos experimentales"])
-%     box on; grid on
-%     legend('Interpreter', 'Latex', 'location', 'SouthWest')
-%     xlabel('$V$ [V]','Interpreter','latex');
-%     ylabel({'$I$';'[A]'},'Interpreter','latex');
-%     ylim([0 0.6])
-%     %Save_as_PDF(h_, ['Figuras/KyH_datos'],'horizontal');
-%     
-% h_ = figure(2);
-%     hold on
-%     plot(V_mess(1:25:end), I_mess(1:25:end), 'o', 'LineWidth', 0.5, 'MarkerSize', 5, 'Color', 'k','DisplayName', ...
-%         ["Datos experimentales"])
-%     plot(puntos_x_sheet, puntos_y_sheet, '^', 'Linewidth', 1, 'Color', 'k', 'DisplayName', ["Puntos caracter\'isticos: data sheet"])
-%     plot(V_mess, I_Karmalkar_analytic_datasheet, '-', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
-%         ["Karmalkar \& Haneefa: data sheet"])
-%     box on; grid on
-%     legend('Interpreter', 'Latex', 'location', 'SouthWest')
-%     xlabel('$V$ [V]','Interpreter','latex');
-%     ylabel({'$I$';'[A]'},'Interpreter','latex');
-%     ylim([0 0.6])
-%     %Save_as_PDF(h_, ['Figuras/KyH_sheet'],'horizontal');
-%     
-% h_ = figure(3);
-%     hold on
-%     plot(V_mess(1:25:end), I_mess(1:25:end), 'o', 'LineWidth', 0.5, 'MarkerSize', 5, 'Color', 'k','DisplayName', ...
-%         ["Datos experimentales"])
-%     plot(V_mess, I_Karmalkar_analytic_exp, '--', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
-%         ["Karmalkar \& Haneefa: datos experimentales"])
-%     plot(V_mess, I_Karmalkar_analytic_datasheet, '-', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
-%         ["Karmalkar \& Haneefa: data sheet"])
-%     box on; grid on
-%     legend('Interpreter', 'Latex', 'location', 'SouthWest')
-%     xlabel('$V$ [V]','Interpreter','latex');
-%     ylabel({'$I$';'[A]'},'Interpreter','latex');
-%     ylim([0 0.6])
-%     %Save_as_PDF(h_, ['Figuras/KyH_ambos'],'horizontal');
+h_ = figure(3);
+    hold on 
+    plot(V_mess(1:25:end), I_mess(1:25:end), 'o', 'LineWidth', 0.5, 'MarkerSize', 5, 'Color', 'k','DisplayName', ...
+        ["Datos experimentales"])
+    plot(puntos_x_datos, puntos_y_datos, '^', 'Linewidth', 1,'Color', 'k', 'DisplayName', ["Puntos caracter\'isticos: datos experimentales"])
+    plot(V_mess, I_Karmalkar_analytic_exp, '-', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
+        ["Karmalkar \& Haneefa: datos experimentales"])
+    box on; grid on
+    legend('Interpreter', 'Latex', 'location', 'SouthWest')
+    xlabel('$V$ [V]','Interpreter','latex');
+    ylabel({'$I$';'[A]'},'Interpreter','latex');
+    ylim([0 0.6])
+    %Save_as_PDF(h_, ['Figuras/KyH_datos'],'horizontal');
+    
+h_ = figure(4);
+    hold on
+    plot(V_mess(1:25:end), I_mess(1:25:end), 'o', 'LineWidth', 0.5, 'MarkerSize', 5, 'Color', 'k','DisplayName', ...
+        ["Datos experimentales"])
+    plot(puntos_x_sheet, puntos_y_sheet, '^', 'Linewidth', 1, 'Color', 'k', 'DisplayName', ["Puntos caracter\'isticos: $datasheet$"])
+    plot(V_mess, I_Karmalkar_analytic_datasheet, '-', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
+        ["Karmalkar \& Haneefa: $datasheet$"])
+    box on; grid on
+    legend('Interpreter', 'Latex', 'location', 'SouthWest')
+    xlabel('$V$ [V]','Interpreter','latex');
+    ylabel({'$I$';'[A]'},'Interpreter','latex');
+    ylim([0 0.6])
+    %Save_as_PDF(h_, ['Figuras/KyH_sheet'],'horizontal');
+    
+h_ = figure(5);
+    hold on
+    plot(V_mess(1:25:end), I_mess(1:25:end), 'o', 'LineWidth', 0.5, 'MarkerSize', 5, 'Color', 'k','DisplayName', ...
+        ["Datos experimentales"])
+    plot(V_mess, I_Karmalkar_analytic_exp, '--', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
+        ["Karmalkar \& Haneefa: datos experimentales"])
+    plot(V_mess, I_Karmalkar_analytic_datasheet, '-', 'LineWidth', 1, 'Color', 'k','DisplayName', ...
+        ["Karmalkar \& Haneefa: $datasheet$"])
+    box on; grid on
+    legend('Interpreter', 'Latex', 'location', 'SouthWest')
+    xlabel('$V$ [V]','Interpreter','latex');
+    ylabel({'$I$';'[A]'},'Interpreter','latex');
+    ylim([0 0.6])
+    %Save_as_PDF(h_, ['Figuras/KyH_ambos'],'horizontal');
