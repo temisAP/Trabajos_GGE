b33c69f530ff0b1a2143e0235d858357549edb88
Danieldelriovelilla <daniel_drv@hotmail.es>
2021-04-14 10:26:10 +0200

    20210414_1026

--- Trabajo_5/Data/Extraer_Data.m
+++ Trabajo_5/Data/Extraer_Data.m
@@ -6,18 +6,18 @@ close all
 %% LEER DATOS
 
 filename = 'ensayos_bateria.xlsx';
-sheets = [1:6];
-fields = {'D5A', 'C5A', 'D2d5A', 'C2d5A', 'D1d5A', 'C1d5A'};
+sheets = [1:2:5, 2:2:6];
+fields = {'D5A', 'D2d5A', 'D1d5A', 'C5A', 'C2d5A', 'C1d5A'};
 xlRange = 'B2:C3';
 
 Bateria = struct();
-for f = 1:length(sheets)
-    Bateria.(fields{f}) = xlsread(filename,sheets(f));
+for s = 1:length(sheets)
+    Bateria.(fields{s}) = xlsread(filename,sheets(s));
     
-    figure(f)
+    figure(s)
         hold on
-        plot(Bateria.(fields{f})(:,1), Bateria.(fields{f})(:,3))
-        title(fields{f})
+        plot(Bateria.(fields{s})(:,1), Bateria.(fields{s})(:,3))
+        title(fields{s})
 end
 
 
@@ -32,31 +32,24 @@ Bateria.C1d5A(1,:) = [];
 
 
 %%
-for f = 1:length(sheets)
-    figure(f)
-        plot(Bateria.(fields{f})(:,1), Bateria.(fields{f})(:,3))
-        title(fields{f})
+for s = 1:length(sheets)
+    figure(s)
+        plot(Bateria.(fields{s})(:,1), Bateria.(fields{s})(:,3))
+        title(fields{s})
 end
 
 
 %% GUARDAR DATOS DE LA BATERIA CON CAMPOS
 
 Ensayos_Bateria = struct();
-ic = [5, 5, 2.5, 2.5, 1.5, 1.5];
+ic = [5, 2.5, 1.5, 5, 2.5, 1.5];
 
-for f = 1:length(sheets)
-    Ensayos_Bateria.(fields{f}).t = Bateria.(fields{f})(:,1);
-    Ensayos_Bateria.(fields{f}).I = Bateria.(fields{f})(:,2);
-    Ensayos_Bateria.(fields{f}).It = Bateria.(fields{f})(:,1)*ic(f);
-    Ensayos_Bateria.(fields{f}).V = Bateria.(fields{f})(:,3);
+for s = 1:length(sheets)
+    Ensayos_Bateria.(fields{s}).t = Bateria.(fields{s})(:,1)/3600;
+    Ensayos_Bateria.(fields{s}).I = Bateria.(fields{s})(:,2);
+    Ensayos_Bateria.(fields{s}).It = Ensayos_Bateria.(fields{s}).t*ic(s);
+    Ensayos_Bateria.(fields{s}).V = Bateria.(fields{s})(:,3);
     
 end
 
-
-
-
 save('Data_Ensayos_Bateria.mat', 'Ensayos_Bateria', 'fields', 'ic');
-
-
-
-
--- Trabajo_5/main.m
+++ Trabajo_5/main.m
@@ -11,12 +11,29 @@ end
 
 
 %% PLOTEAR CURVAS CARGA Y DESCARGA
+
+color = [0, 0.4470, 0.7410;
+         0.8500, 0.3250, 0.0980;
+         0.4940, 0.1840, 0.5560];
+c = 0;
+
 figure()
     hold on
 for f = 1:2:length(fields)
+    c = c+1;
+    p = polyfit(Ensayos_Bateria.(fields{f}).It,Ensayos_Bateria.(fields{f}).V,1);
+    Ensayos_Bateria.(fields{f}).Lineal = ...
+        polyval(p,Ensayos_Bateria.(fields{f}).It);
+    
+    % Plots
     plot(Ensayos_Bateria.(fields{f}).It,...
-         Ensayos_Bateria.(fields{f}).V, 'DisplayName', fields{f})
+         Ensayos_Bateria.(fields{f}).V, 'LineWidth', 1.5,...
+         'Color', color(c,:), 'DisplayName', fields{f})
+    plot(Ensayos_Bateria.(fields{f}).It,...
+         Ensayos_Bateria.(fields{f}).Lineal, '--', 'LineWidth', 1.5,...
+         'Color', color(c,:), 'DisplayName', fields{f})
 end
+    grid on; box on;
     legend('Interpreter', 'Latex', 'Location', 'Best')
     title('Descarga')
     
@@ -28,3 +45,69 @@ for f = 2:2:length(fields)
 end
     legend('Interpreter', 'Latex', 'Location', 'Best')
     title('Descarga')
+    
+    
+%% CALCULO DE Rc,d
+
+It = [1, 2, 3]*1e4;
+% Carga
+for t = 1:3
+    
+    i = 0;
+    for f = 1:2:length(fields)
+
+        i = i + 1;
+
+        [val,idx] = min(abs(Ensayos_Bateria.(fields{f}).It - It(t)));
+        V(i) = Ensayos_Bateria.(fields{f}).V(idx);
+        I(i) = ic(f);
+
+    end
+    
+    Rc(t) = mean( (V(3) - V(2))/(I(2) - I(3)) + (V(2) - V(1))/(I(1) - I(2)) );
+end
+
+Rc = mean(Rc);
+
+% Descarga
+for t = 1:3
+    
+    i = 0;
+    for f = 2:2:length(fields)
+
+        i = i + 1;
+
+        [val,idx] = min(abs(Ensayos_Bateria.(fields{f}).It - It(t)));
+        V(i) = Ensayos_Bateria.(fields{f}).V(idx);
+        I(i) = ic(f);
+
+    end
+    
+    Rd(t) = mean( (V(2) - V(3))/(I(2) - I(3)) + (V(1) - V(2))/(I(1) - I(2)) );
+end
+
+Rd = mean(Rd);
+
+
+%% PHI
+
+figure()
+    hold on
+    c = 0;
+for f = 1:2:length(fields)
+    c = c + 1;
+    Ensayos_Bateria.(fields{f}).phi = ...
+        Ensayos_Bateria.(fields{f}).V.*ic(f)- Rc*ic(f)^2;
+    
+    plot(Ensayos_Bateria.(fields{f}).phi.*Ensayos_Bateria.(fields{f}).t,...
+         Ensayos_Bateria.(fields{f}).V, 'LineWidth', 1.5,...
+         'Color', color(c,:), 'DisplayName', fields{f})
+end
+
+for f = 2:2:length(fields)
+    Ensayos_Bateria.(fields{f}).phi = ...
+        Ensayos_Bateria.(fields{f}).V.*ic(f)- Rd*ic(f)^2;
+end
+
+
+
