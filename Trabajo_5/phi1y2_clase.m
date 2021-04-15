a412f211ab5c8a7895409419a8829810f44ef188
temisAP <74715759+temisAP@users.noreply.github.com>
2021-04-15 19:11:26 +0200

    filnlm

--- Trabajo_5/phi1y2_clase.m
+++ Trabajo_5/phi1y2_clase.m
@@ -1,11 +1,11 @@
 %% Primera estimación de la energía descargada
 
 for f = 1:2:length(Ensayos_Bateria)
-    
+
     Id = Ensayos_Bateria.(fields{f}).I;
     V = Ensayos_Bateria.(fields{f}).V;
     time = Ensayos_Bateria.(fields{f}).t;
-    
+
     phi1(1) = 0;
     phi2(1) = 0;
     for t = 2:length(time)
@@ -15,17 +15,17 @@ for f = 1:2:length(Ensayos_Bateria)
     end
     Ensayos_Bateria.(fields{f}).phi1 = phi1';
     Ensayos_Bateria.(fields{f}).phi2 = phi2';
-    
+
     clear Id V time t Dt phi1 phi2 %Esto es para luego no tener 1000 variables
-    
+
 end
 
 %% Modelo lineal
 
-% Fit no linear model: 
+% Fit no linear model:
 % Vector de pesos para datos descompensados
 
 %% Modelo exponencial
 
-% Fit no linear model: 
-% 
+% Fit no linear model:
+%
