5b1dd307a3ec4231170370d80c4e124144bd0796
temisAP <74715759+temisAP@users.noreply.github.com>
2021-04-15 19:29:30 +0200

    main
    descargar cambios

--- Trabajo_5/main.m
+++ Trabajo_5/main.m
@@ -17,76 +17,44 @@ color = [0, 0.4470, 0.7410;
          0.4940, 0.1840, 0.5560];
 c = 0;
 
+% Descarga
+
 figure()
     hold on
-for f = 1:2:length(fields)
+for f = 1:length(Descarga)
     c = c+1;
-    p = polyfit(Ensayos_Bateria.(fields{f}).It,Ensayos_Bateria.(fields{f}).V,1);
-    Ensayos_Bateria.(fields{f}).Lineal = ...
-        polyval(p,Ensayos_Bateria.(fields{f}).It);
+    p = polyfit(Descarga(f).It,Descarga(f).V,1);
+    Descarga(f).Lineal = ...
+        polyval(p,Descarga(f).It);
 
     % Plots
-    plot(Ensayos_Bateria.(fields{f}).It,...
-         Ensayos_Bateria.(fields{f}).V, 'LineWidth', 1.5,...
-         'Color', color(c,:), 'DisplayName', fields{f})
-    plot(Ensayos_Bateria.(fields{f}).It,...
-         Ensayos_Bateria.(fields{f}).Lineal, '--', 'LineWidth', 1.5,...
-         'Color', color(c,:), 'DisplayName', fields{f})
+    plot(Descarga(f).It,...
+         Descarga(f).V, 'LineWidth', 1.5,...
+         'Color', color(c,:), 'DisplayName', Descarga(f).Name)
+    plot(Descarga(f).It,...
+         Descarga(f).Lineal, '--', 'LineWidth', 1.5,...
+         'Color', color(c,:), 'DisplayName', Descarga(f).Name)
 end
     grid on; box on;
     legend('Interpreter', 'Latex', 'Location', 'Best')
     title('Descarga')
 
+% Carga
+
 figure()
     hold on
-for f = 2:2:length(fields)
-    plot(Ensayos_Bateria.(fields{f}).It,...
-         Ensayos_Bateria.(fields{f}).V, 'DisplayName', fields{f})
+for f = 1:length(Carga)
+        plot(Carga(f).It,...
+             Carga(f).V, 'DisplayName', Carga(f).Name)
 end
     legend('Interpreter', 'Latex', 'Location', 'Best')
-    title('Descarga')
+    title('Carga')
 
 
 %% CALCULO DE Rc,d
 
-It = [1, 2, 3]*1e4;
-% Carga
-for t = 1:3
-
-    i = 0;
-    for f = 1:2:length(fields)
-
-        i = i + 1;
-
-        [val,idx] = min(abs(Ensayos_Bateria.(fields{f}).It - It(t)));
-        V(i) = Ensayos_Bateria.(fields{f}).V(idx);
-        I(i) = ic(f);
-
-    end
-
-    Rc(t) = mean( (V(3) - V(2))/(I(2) - I(3)) + (V(2) - V(1))/(I(1) - I(2)) );
-end
-
-Rc = mean(Rc);
-
-% Descarga
-for t = 1:3
-
-    i = 0;
-    for f = 2:2:length(fields)
-
-        i = i + 1;
-
-        [val,idx] = min(abs(Ensayos_Bateria.(fields{f}).It - It(t)));
-        V(i) = Ensayos_Bateria.(fields{f}).V(idx);
-        I(i) = ic(f);
-
-    end
-
-    Rd(t) = mean( (V(2) - V(3))/(I(2) - I(3)) + (V(1) - V(2))/(I(1) - I(2)) );
-end
-
-Rd = mean(Rd);
+Rc = getR(Carga);
+Rd = getR(Descarga);
 
 
 %% PHI
@@ -113,6 +81,37 @@ end
 
 linearmodel = linearbatt(Descarga,Rd)
 
+
+%% R
+
+function  val = getR(data)
+
+  It = [1, 2, 3]*1e4;
+  % Carga
+  for t = 1:3
+
+      i = 0;
+      for f = 1:length(data)
+
+          i = i + 1;
+
+          [val,idx] = min(abs(data(f).It - It(t)));
+          V(i) = data(f).V(idx);
+          I(i) = ic(f);
+
+      end
+
+      Rc(t) = mean( (V(3) - V(2))/(I(2) - I(3)) + (V(2) - V(1))/(I(1) - I(2)) );
+  end
+
+  Rc = mean(Rc);
+
+  val = abs(Rc);
+
+
+end
+
+
 %% Modelo lineal
 
 function val = linearbatt(data,R)
@@ -121,7 +120,7 @@ function val = linearbatt(data,R)
   phi1  = [];
   phi2  = [];
   V     = [];
-  for d=2:length(data)
+  for d = 1:length(data)
     I     = [I ; data(d).I];
     phi1  = [phi1 ; data(d).phi1];
     phi2  = [phi2 ; data(d).phi2];
@@ -130,7 +129,7 @@ function val = linearbatt(data,R)
   MAT = [I,phi1,phi2];
 
   i = 1;
-  for d=1:length(data)
+  for d = 1:length(data)
     weights(i:i+length(data(d))) = length(data(d).V)/length(V);
     i = length(data(d));
   end
@@ -139,8 +138,8 @@ function val = linearbatt(data,R)
 
   beta0 = [max(V) -1.5e-5 R];
 
-  for i=1:5
-    val = fitnlm(MAT, V, myfunction, beta0,'Weights',weights)
+  for i = 1:5
+    val = fitnlm(MAT, V, myfunction, beta0,'Weights',weights);
     beta0(:) = table2array(val.Coefficients(1:3,1));
   end
 
